9de77db2429c857be0c6c522e0450b3171f91470
Marina <80681151+MarinaZagumenova@users.noreply.github.com>
2021-11-16 21:48:48 +0500

    Step_2

--- filter.py
+++ filter.py
@@ -1,37 +1,41 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-arr = np.array(img).astype(int)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 11:
-while i < a:
-    j = 0
-    while j < a1 - 11:
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-            for m in range(j, j + 10):
-                n1 = arr[n][m][0]
-                n2 = arr[n][m][1]
-                n3 = arr[n][m][2]
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-                arr[n][n1][0] = int(s // 50) * 50 / 3
-                arr[n][n1][1] = int(s // 50) * 50 / 3
-                arr[n][n1][2] = int(s // 50) * 50 / 3
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
+
+def get_pixel_image(image, pixel_size = 10, gradation = 5):
+    array_img = np.array(image).astype(int)
+    height_img = len(array_img[0])
+    length_img = len(array_img)
+    gradation_img = 255 // gradation
+    i = j = 0
+    while i < length_img:
+        while j < height_img:
+            colors_sum = get_elem_sum(array_img, pixel_size, i, j)
+            average_color = int(colors_sum // (pixel_size ** 2))
+            set_color(int(average_color // gradation_img) * gradation_img / 3, array_img, pixel_size, i, j)
+            j += pixel_size
+        i += pixel_size
+    return Image.fromarray(np.uint8(array_img))
+
+
+def get_elem_sum(img, pixel_size, i, j):
+    color_sum = 0
+    for sector1 in range(i, i + pixel_size):
+        for sector2 in range(j, j + pixel_size):
+            n1 = img[sector1][sector2][0]
+            n2 = img[sector1][sector2][1]
+            n3 = img[sector1][sector2][2]
+            M = n1 + n2 + n3
+            color_sum += M
+    return color_sum
+
+
+def set_color(new_color, vector, pixel_size, i, j):
+    for sector1 in range(i, i + pixel_size):
+        for sector2 in range(j, j + pixel_size):
+            for c in range(3):
+                vector[sector1][sector2][c] = new_color
+
+
+original_img = Image.open("img2.jpg")
+result = get_pixel_image(image = original_img, pixel_size = int(input()), gradation = int(input()))
+result.save('res.jpg')
