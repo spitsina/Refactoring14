6bb3fa5d43ade4b88655c702749bb6fe72dbdbe1
Кристина Няшина <94453999+KristinaN02@users.noreply.github.com>
2021-11-16 21:10:41 +0500

    этап 2

--- filter.py
+++ filter.py
@@ -1,20 +1,37 @@
 from PIL import Image
+from numpy import *
 import numpy as np
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for k in range(j, j + 10):
-                n1 = arr[n][k][0]
-                n2 = arr[n][k][1]
-                n3 = arr[n][k][2]
-                M = (int(n1) + int(n2) + int(n3)) / 3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
+class MonochromePhoto:
+    def __init__(self, pixArray, pixSize=10, gradation=5):
+        self.image = np.array(pixArray)
+        self.size = pixSize
+        self.gradation = 255 // gradation
+        self.height = len(self.image)
+        self.width = len(self.image[0]) 
+        
+        def get_monochrome_photo(self):
+        for px in range(0, self.width, self.size):
+            for y in range(0, self.height, self.size):
+                self.get_grey_color(color=self.get_medium_color(px, y), px=px, y=y)
+        return Image.fromarray(self.image)
+
+    def get_medium_color(self, px, y):
+        color = 0
+        for column in range(px, px + self.size):
+            for row in range(y, y + self.size):
+                color += sum([int(self.image[column][row][i]) for i in range(3)])
+        return int(color / 3 // (self.size ** 2))
+
+    def get_grey_color(self, color, px, y):
+        for column in range(
+            px, px + self.size):
+            for row in range(y, y + self.size):
+                self.image[column][row][0] = int(color // self.gradation) * self.gradation
+                self.image[column][row][1] = int(color // self.gradation) * self.gradation
+                self.image[column][row][2] = int(color // self.gradation) * self.gradation
+
+
+initialImage = Image.open("img2.jpg")
+result = MonochromePhoto(pixArray=initialImage, pixSize=int(input()), gradation=int(input())).get_monochrome_photo()
+result.save('res.jpg')
