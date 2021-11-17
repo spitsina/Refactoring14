2ef862289db4b70a0819ccc676793d366c017954
Marina <80681151+MarinaZagumenova@users.noreply.github.com>
2021-11-17 17:47:54 +0500

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,5 +1,7 @@
 from PIL import Image
 import numpy as np
+import sys
+
 def get_pixel_image(image, pixel_size = 10, gradation = 5):
     gradation_img = 255 // gradation
     array_img = np.array(image).astype(int)
@@ -16,12 +18,18 @@ def get_pixel_image(image, pixel_size = 10, gradation = 5):
         i += pixel_size
     return Image.fromarray(np.uint8(array_img))
 
+
 def set_color(new_color, vector, pixel_size, i, j):
     for sector1 in range(i, i + pixel_size):
         for sector2 in range(j, j + pixel_size):
-            for c in range(3):
-                vector[sector1][sector2][c] = new_color
+            for n in range(3):
+                vector[sector1][sector2][n] = new_color
                 
-original_img = Image.open("img2.jpg")
-result = get_pixel_image(image = original_img, pixel_size = int(input()), gradation = int(input()))
-result.save('res.jpg')
+
+if __name__ == "__main__":
+    source_name = sys.argv[1]
+    output_name = sys.argv[2]
+
+    source_img = Image.open(source_name)
+    array_img = np.array(source_img).astype(int)
+    get_pixel_image(array_img, int(input()), int(input())).save(output_name)
