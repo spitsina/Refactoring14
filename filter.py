b0b866d0757174f2e4300a17117a27775a3ecc5d
bda-nt <74663073+bda-nt@users.noreply.github.com>
2021-11-17 17:29:51 +0500

    3 and 4 commit
    third and second task

--- filter.py
+++ filter.py
@@ -11,24 +11,14 @@ def get_mosaice_image(image, mosaice_size=10, gradation=4):
     while i < image_length - 1:
         j = 0
         while j < image_height - 1:
-            matrix_sum = get_matrix_sum(processed_image, mosaice_size, i, j)
-            average_color = int(matrix_sum // (mosaice_size ** 2))
+            sector = processed_image[i: i + mosaice_size, j: j + mosaice_size]
+            color_sum = np.sum(sector)
+            average_color = int(color_sum // (mosaice_size ** 2))
             set_color(int(average_color // threshold) * threshold / 3, processed_image, i, j, mosaice_size)
             j += mosaice_size
         i += mosaice_size
 
     return Image.fromarray(np.uint8(processed_image))
-
-
-def get_matrix_sum(source_color, size, i, j, ):
-    color_sum = 0
-    for sector_i in range(i, i + size):
-        for sector_j in range(j, j + size):
-            M = 0
-            for color_id in range(3):
-                M += source_color[sector_i][sector_j][color_id]
-            color_sum += M
-    return color_sum
 
 
 def set_color(new_color, source_color, i, j, size):
@@ -39,6 +29,7 @@ def set_color(new_color, source_color, i, j, size):
 
 
 if __name__ == "__main__":
-    img = Image.open("img2.jpg")
-    res = get_mosaice_image(img)
-    res.save('res.jpg')
+    img = Image.open(f"{input('Input image name:')}.jpg")
+    res = get_mosaice_image(img, gradation= 6)
+    img_res = input("Input name result:")
+    res.save(f"{ img_res }.jpg")
